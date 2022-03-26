d7e39d27b5ae8aa144f0524bc1e7aa43b4bdfb1f
Juan Pablo Barreto <juampi92@gmail.com>
2022-03-26 01:41:12 +0100

    fix JsonResource import on tests

--- tests/Fixtures/Resources/App/v1/Posts/Single.php
+++ tests/Fixtures/Resources/App/v1/Posts/Single.php
@@ -4,9 +4,9 @@ namespace Juampi92\APIResources\Tests\Fixtures\Resources\App\v1\Posts;
 
 use JsonSerializable;
 use Juampi92\APIResources\Tests\Fixtures\Arrayable;
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class Single extends Resource implements Arrayable
+class Single extends JsonResource implements Arrayable
 {
   public function toArray($request)
   {
--- tests/Fixtures/Resources/App/v2/Post.php
+++ tests/Fixtures/Resources/App/v2/Post.php
@@ -4,9 +4,9 @@ namespace Juampi92\APIResources\Tests\Fixtures\Resources\App\v2;
 
 use JsonSerializable;
 use Juampi92\APIResources\Tests\Fixtures\Arrayable;
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class Post extends Resource implements Arrayable
+class Post extends JsonResource implements Arrayable
 {
   public function toArray($request)
   {
